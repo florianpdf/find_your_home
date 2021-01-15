1350bc5d637457fb3cd3b1d6653490a0074b1262
florianpdf <florian.pdf@gmail.com>
2021-01-15 14:18:05 +0100

    Add base project

--- src/index.js
+++ src/index.js
@@ -10,50 +10,10 @@ const render = (array) => {
   });
 };
 
-const checkIfEmpty = (array) => {
-  if (array.length === 0) {
-    cards.innerHTML = 'No house';
-  } else {
-    render(array);
-  }
-}
-
 // On change select (all / flat / house)
-const select = document.querySelector('.select');
-
-select.addEventListener('click', () => {
-  const userInput = select.value;
-  if (userInput === 'All') {
-    render(houseToRent);
-  } else {
-    const houses = houseToRent.filter((house) => house.type === userInput);
-    checkIfEmpty(houses);
-  }
-});
 
 // On search (full text search)
-const textInput = document.querySelector('.search-input');
-textInput.addEventListener('input', () => {
-  const userValueInput = textInput.value.toLowerCase();
-  const houses = houseToRent.filter((house) => house.name.toLowerCase().includes(userValueInput));
-  checkIfEmpty(houses);
-});
 
 // On change (Show only available)
-const checkbox = document.querySelector('.available-checkbox');
-checkbox.addEventListener('click', () => {
-  const checkboxValue = checkbox.checked;
-  if (checkboxValue) {
-    const houses = houseToRent.filter((house) => house.available);
-    checkIfEmpty(houses);
-  } else {
-    render(houseToRent);
-  }
-});
 
 // If all house is rent, show a specific message
-if (houseToRent.every((house) => !house.available)) {
-  cards.innerHTML = 'No available house';
-} else {
-  render(houseToRent);
-}
